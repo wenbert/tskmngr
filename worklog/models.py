89878b96a769d4a2fedf24556bed41164da6c597
Wenbert Del Rosario <wenbert@gmail.co>
2011-09-12 07:58:59 +0200

    added worklog app and gitignore

---
+++ worklog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Worklog(models.Model):
+    unique_id = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    desc  = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+class Logdetail(models.Model):
+    worklog     = models.ForeignKey(Worklog)
+    log_date    = models.DateField('Date')
+    log_start   = models.TimeField('Start Time')
+    log_end     = models.TimeField('End Time')
+    log_hours   = models.DecimalField('Total Hours', \
+                    max_digits=19, decimal_places=2)
+    log_amount  = models.DecimalField('Line Amount', \
+                    max_digits=19, decimal_places=2)
+    comments    = modeks.TextField()
+    
